c835c5599c9bffa2a247ab432fd7587a712009bb
action <action@github.com>
2025-07-17 08:14:41 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Jul 17 2025 08:09:29 GMT+0000 (Coordinated Universal Time)
+//Thu Jul 17 2025 08:14:41 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 function restoreUrl(_0x23f89b) {
--- output.py
+++ output.py
@@ -1,4 +1,4 @@
-#2025-07-17 08:09:20
+#2025-07-17 08:14:34
 import requests
 import os
 import time
